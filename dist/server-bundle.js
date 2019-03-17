57f732026eaa90a62272b728dfb582063c1f468e
Rishi Karri <rishikarri1@gmail.com>
2019-03-17 13:00:44 -0400

    collapse dist

--- dist/server-bundle.js
+++ dist/server-bundle.js
@@ -28645,7 +28645,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)(); // app.use("/", express.static("../client-dist" ));
 
-app.use(_express2.default.static("client-dist"));
+app.use(_express2.default.static("dist/client-dist"));
 console.log(_path2.default.resolve(__dirname, "../client-dist"), '%#%#%#%#%');
 app.get("/", function (req, res) {
   var jsx = _react2.default.createElement(_Layout2.default, null);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 import express from "express";
-import path from "path";
-
 import React from "react";
 import { renderToString } from "react-dom/server";
 import Layout from "./components/Layout";
 
 const app = express();
 
-// app.use("/", express.static("../client-dist" ));
-app.use( express.static( "client-dist" ));
-console.log(path.resolve( __dirname, "../client-dist" ), '%#%#%#%#%');
+app.use( express.static( "dist/client-dist" ));
+
 app.get( "/", ( req, res ) => {
     const jsx = ( <Layout /> );
     const reactDom = renderToString( jsx );
@@ -18,21 +15,9 @@ app.get( "/", ( req, res ) => {
     res.end( htmlTemplate( reactDom ) );
 } );
 
-// app.get( "/client-bundle.js", ( req, res ) => {
-
-
-//     res.writeHead( 200, { "Content-Type": "text/javascript" } );
-//     res.end( 'hi' );
-// } );
-
 app.listen( 2048 );
 
 function htmlTemplate( reactDom ) {
-    // function hi() {
-    //     return () => {console.log('hi') }
-    // }
-    // const hello = hi;
-    // hello();
     return `
        <!DOCTYPE html>
         <html>
